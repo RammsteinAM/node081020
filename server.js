508f38449bfada017917ff6ffb822bae5bad82b7
Avet Brutyan <awet.brutjan@gmail.com>
2020-10-12 03:31:43 +0400

    Minor fixes

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const http = require("http");
 const express = require("express");
 const path = require("path");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 const port = 8080;
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 app.use(cookieParser());
 
 app.use(
   express.urlencoded({
-    extended: true
+    extended: true,
   })
 );
 
@@ -23,63 +23,55 @@ app.use(bodyParser.json());
 const todos = [];
 
 app.get("/", (req, res) => {
-  res.render('index', { title: 'Todos', todos });
+  res.render("index", { title: "Todos", todos });
 });
 
 app.post("/create", (req, res, next) => {
   const todo = {
     id: uuidv4(),
-    text: req.body.text
+    text: req.body.text,
   };
-  if(todo.id && todo.text) {
+  if (!todo.id || !todo.text) {
+    next({ status: 400, message: "Invalid data." });
+  } else {
     todos.unshift(todo);
-    res.status(201).redirect('/');
-  }
-  else {
-    next({ status: 400, text: "Missing data." });
+    res.status(201).redirect("/");
   }
 });
 
 app.get("/edit/:id", (req, res, next) => {
-  const foundIndex = todos.findIndex(item => 
-    item.id === req.params.id
-  );
-  if (foundIndex >= 0) {    
-    res.render('edit', { title: 'Edit Item', item: todos[foundIndex] });
-  }
-  else {
-    next({ status: 404 });
+  const foundIndex = todos.findIndex((item) => item.id === req.params.id);
+  if (foundIndex < 0) {
+    next({ status: 404, message: "Item not found." });
+  } else {
+    res.render("edit", { title: "Edit Item", item: todos[foundIndex] });
   }
 });
 
 app.post("/edit/:id", (req, res, next) => {
-  const foundIndex = todos.findIndex(item => 
-    item.id === req.params.id
-  );
-  if(todos[foundIndex] && req.body.text) {
+  const foundIndex = todos.findIndex((item) => item.id === req.params.id);
+  if (foundIndex < 0) {
+    next({ status: 404, message: "Item not found." });
+  } else if (!req.body.text) {
+    next({ status: 400, message: "Invalid data." });
+  } else {
     todos[foundIndex].text = req.body.text;
-    res.status(204).redirect('/');
-  }
-  else {
-    next({ status: 400, text: "Missing data." });
+    res.status(204).redirect("/");
   }
 });
 
 app.delete("/delete/:id", (req, res, next) => {
-  const foundIndex = todos.findIndex(item => 
-    item.id === req.params.id
-  );
-  if (foundIndex >= 0) {
-    todos.splice(foundIndex, 1);    
+  const foundIndex = todos.findIndex((item) => item.id === req.params.id);
+  if (foundIndex < 0) {
+    next({ status: 404, message: "Item not found." });
+  } else {
+    todos.splice(foundIndex, 1);
     res.sendStatus(204);
-  }
-  else {
-    next({ status: 404 })
   }
 });
 
 app.use((err, req, res, next) => {
-  res.status(err.status || 500).render('error', err );
-})
+  res.status(err.status || 500).render("error", err);
+});
 
 app.listen(port);
